553fe5d74efefae28000352919fed01c2177feff
Allen <allenvs@mail.ru>
2020-04-12 11:57:42 +0300

    added all models to admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Pizza, Pizza_name, Pizza_size, Pizza_topping, Pizza_topping_combo, Sub, Sub_name, Sub_size, Sub_add_on
+from .models import Pizza, Pizza_name, Pizza_size, Pizza_topping, Pizza_topping_combo, Sub, Sub_name, Sub_size, Sub_add_on, Pasta, Salad, Dinner_platter, Order
 
 
 # Register your models here.
@@ -13,3 +13,7 @@ admin.site.register(Sub)
 admin.site.register(Sub_name)
 admin.site.register(Sub_size)
 admin.site.register(Sub_add_on)
+admin.site.register(Pasta)
+admin.site.register(Salad)
+admin.site.register(Dinner_platter)
+admin.site.register(Order)
