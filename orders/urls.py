00808595390d49c178f824d3ca21e5e1b7d02945
Allen <allenvs@mail.ru>
2020-05-05 23:00:02 +0300

    added base.html template and updated index.html

--- orders/urls.py
+++ orders/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("register", views.register_view, name ="register")
+    path("", views.index, name="index"), # Links to Login page and Register page, if signed in - redirect to user page
+    path("register", views.register_view, name ="register"), # reg form, once registered - to login page, other - to error page
+    path("login", views.login_view, name="login"), # login form, onced signed-in - to index, other - to login page with error
+    path("logout", views.logout_view, name="logout") # to login page with message "logged out"
 ]
