729086bd7f38d2a35f909f9e0ed6110451aa5276
Allen <allenvs@mail.ru>
2020-06-20 16:38:44 +0300

    added the first form change event handler

--- orders/static/orders/app.js
+++ orders/static/orders/app.js
@@ -1,1 +1,12 @@
-console.log("helloworld");
+document.addEventListener('DOMContentLoaded', () =>{
+    console.log("on page load event");
+    const pizza_form=document.querySelector('#pizza_form');
+    pizza_form.addEventListener('change', get_price);
+      
+});
+
+function get_price() {
+    
+    console.log("pizza_form change event");
+    
+};
